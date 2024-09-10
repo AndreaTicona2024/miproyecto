8cc78ed05b421cf4fa3cbcf641aa14bd8ca243d5
AndreaTicona2024 <aguilar.andrea.8026@gmail.com>
2024-09-09 23:58:21 -0400

    implementacion de categoria y act. produc

--- app/Models/articulo.php
+++ app/Models/articulo.php
@@ -30,3 +30,28 @@ public function activar($idarticulo){
 	return ejecutarConsulta($sql);
 }
 
+//metodo para mostrar registros
+public function mostrar($idarticulo){
+	$sql="SELECT * FROM articulo WHERE idarticulo='$idarticulo'";
+	return ejecutarConsultaSimpleFila($sql);
+}
+
+//listar registros 
+public function listar(){
+	$sql="SELECT a.idarticulo,a.idcategoria,c.nombre as categoria,a.codigo, a.nombre,a.stock,a.descripcion,a.imagen,a.condicion FROM articulo a INNER JOIN Categoria c ON a.idcategoria=c.idcategoria";
+	return ejecutarConsulta($sql);
+}
+
+//listar registros activos
+public function listarActivos(){
+	$sql="SELECT a.idarticulo,a.idcategoria,c.nombre as categoria,a.codigo, a.nombre,a.stock,a.descripcion,a.imagen,a.condicion FROM articulo a INNER JOIN Categoria c ON a.idcategoria=c.idcategoria WHERE a.condicion='1'";
+	return ejecutarConsulta($sql);
+}
+
+//implementar un metodo para listar los activos, su ultimo precio y el stock(vamos a unir con el ultimo registro de la tabla detalle_ingreso)
+public function listarActivosVenta(){
+	$sql="SELECT a.idarticulo,a.idcategoria,c.nombre as categoria,a.codigo, a.nombre,a.stock,(SELECT precio_venta FROM detalle_ingreso WHERE idarticulo=a.idarticulo ORDER BY iddetalle_ingreso DESC LIMIT 0,1) AS precio_venta,a.descripcion,a.imagen,a.condicion FROM articulo a INNER JOIN Categoria c ON a.idcategoria=c.idcategoria WHERE a.condicion='1'";
+	return ejecutarConsulta($sql);
+}
+}
+ ?>
