cdc49735fba3e756896c03eb707dd17696de728f
AndreaTicona2024 <aguilar.andrea.8026@gmail.com>
2024-09-11 14:59:10 -0400

    categoria modificado

--- app/Views/inc/categoria.php
+++ app/Views/inc/categoria.php
@@ -46,3 +46,43 @@ if ($_SESSION['almacen']==1) {
     </tfoot>   
   </table>
 </div>
+<div class="panel-body" style="height: 400px;" id="formularioregistros">
+  <form action="" name="formulario" id="formulario" method="POST">
+    <div class="form-group col-lg-6 col-md-6 col-xs-12">
+      <label for="">Nombre</label>
+      <input class="form-control" type="hidden" name="idcategoria" id="idcategoria">
+      <input class="form-control" type="text" name="nombre" id="nombre" maxlength="50" placeholder="Nombre" required>
+    </div>
+        <div class="form-group col-lg-6 col-md-6 col-xs-12">
+      <label for="">Descripcion</label>
+      <input class="form-control" type="text" name="descripcion" id="descripcion" maxlength="256" placeholder="Descripcion">
+    </div>
+    <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+      <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"></i>  Guardar</button>
+
+      <button class="btn btn-danger" onclick="cancelarform()" type="button"><i class="fa fa-arrow-circle-left"></i> Cancelar</button>
+    </div>
+  </form>
+</div>
+<!--fin centro-->
+      </div>
+      </div>
+      </div>
+      <!-- /.box -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+<?php 
+}else{
+ require 'noacceso.php'; 
+}
+
+require 'footer.php';
+ ?>
+ <script src="scripts/categoria.js"></script>
+ <?php 
+}
+
+ob_end_flush();
+  ?>
