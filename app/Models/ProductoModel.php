dfe2799607f80702695d518db73dbbcf9953baab
AndreaTicona2024 <aguilar.andrea.8026@gmail.com>
2024-09-14 11:30:54 -0400

    configuracion en producto y categoria

--- app/Models/ProductoModel.php
+++ app/Models/ProductoModel.php
@@ -1,63 +1,54 @@
-<?php
+<?php 
+//incluir la conexion de base de datos
+require "../config/Conexion.php";
+class Articulo{
 
-<<<<<<< HEAD
-namespace App\Models;
+public function __construct(){
 
-use CodeIgniter\Model;
-
-class Productos extends Model
-=======
-namespace App\Controllers;
-
-use App\Models\ProductoModel;
-
-class ProductosController extends BaseController
->>>>>>> df995b144dcec79aa9274b344d523ee119dfc1c3
-{
-    public function crear()
-    {
-        return view('create_producto');
-    }
-
-    public function guardar()
-    {
-        $productoModel = new ProductoModel();
-
-        // Reglas de validación
-        $validation = \Config\Services::validation();
-
-        if (!$this->validate([
-            'nombre' => 'required|min_length[3]',
-            'descripcion' => 'required',
-            'precioBase' => 'required|decimal',
-            'stock' => 'required|integer',
-            'fechaRegistro' => 'required|valid_date',
-            'estado' => 'required|in_list[activo,inactivo]',
-            'imgUrl' => 'uploaded[imgUrl]|max_size[imgUrl,2048]|is_image[imgUrl]'
-        ])) {
-            return redirect()->back()->withInput()->with('errors', $validation->getErrors());
-        }
-
-        $imgUrl = $this->request->getFile('imgUrl');
-        $imgUrlName = $imgUrl->getRandomName();
-
-        if ($imgUrl->isValid() && !$imgUrl->hasMoved()) {
-            $imgUrl->move(ROOTPATH . 'public/uploads', $imgUrlName);
-        }
-
-        $data = [
-            'nombre' => $this->request->getPost('nombre'),
-            'descripcion' => $this->request->getPost('descripcion'),
-            'precioBase' => $this->request->getPost('precioBase'),
-            'stock' => $this->request->getPost('stock'),
-            'fechaRegistro' => $this->request->getPost('fechaRegistro'),
-            'fechaActualizacion' => $this->request->getPost('fechaActualizacion'),
-            'estado' => $this->request->getPost('estado'),
-            'imgUrl' => $imgUrlName
-        ];
-
-        $productoModel->save($data);
-
-        return redirect()->to(site_url('productos'));
-    }
 }
+
+public function insertar($idcategoria,$codigo,$nombre,$stock,$descripcion,$imagen){
+	$sql="INSERT INTO articulo (idcategoria,codigo,nombre,stock,descripcion,imagen,condicion)
+	 VALUES ('$idcategoria','$codigo','$nombre','$stock','$descripcion','$imagen','1')";
+	return ejecutarConsulta($sql);
+}
+
+public function editar($idarticulo,$idcategoria,$codigo,$nombre,$stock,$descripcion,$imagen){
+	$sql="UPDATE articulo SET idcategoria='$idcategoria',codigo='$codigo', nombre='$nombre',stock='$stock',descripcion='$descripcion',imagen='$imagen' 
+	WHERE idarticulo='$idarticulo'";
+	return ejecutarConsulta($sql);
+}
+public function desactivar($idarticulo){
+	$sql="UPDATE articulo SET condicion='0' WHERE idarticulo='$idarticulo'";
+	return ejecutarConsulta($sql);
+}
+public function activar($idarticulo){
+	$sql="UPDATE articulo SET condicion='1' WHERE idarticulo='$idarticulo'";
+	return ejecutarConsulta($sql);
+}
+
+//metodo para mostrar registros
+public function mostrar($idarticulo){
+	$sql="SELECT * FROM articulo WHERE idarticulo='$idarticulo'";
+	return ejecutarConsultaSimpleFila($sql);
+}
+
+//listar registros 
+public function listar(){
+	$sql="SELECT a.idarticulo,a.idcategoria,c.nombre as categoria,a.codigo, a.nombre,a.stock,a.descripcion,a.imagen,a.condicion FROM articulo a INNER JOIN Categoria c ON a.idcategoria=c.idcategoria";
+	return ejecutarConsulta($sql);
+}
+
+//listar registros activos
+public function listarActivos(){
+	$sql="SELECT a.idarticulo,a.idcategoria,c.nombre as categoria,a.codigo, a.nombre,a.stock,a.descripcion,a.imagen,a.condicion FROM articulo a INNER JOIN Categoria c ON a.idcategoria=c.idcategoria WHERE a.condicion='1'";
+	return ejecutarConsulta($sql);
+}
+
+//implementar un metodo para listar los activos, su ultimo precio y el stock(vamos a unir con el ultimo registro de la tabla detalle_ingreso)
+public function listarActivosVenta(){
+	$sql="SELECT a.idarticulo,a.idcategoria,c.nombre as categoria,a.codigo, a.nombre,a.stock,(SELECT precio_venta FROM detalle_ingreso WHERE idarticulo=a.idarticulo ORDER BY iddetalle_ingreso DESC LIMIT 0,1) AS precio_venta,a.descripcion,a.imagen,a.condicion FROM articulo a INNER JOIN Categoria c ON a.idcategoria=c.idcategoria WHERE a.condicion='1'";
+	return ejecutarConsulta($sql);
+}
+}
+ ?>
