b32f41facc2ff4c98941daf1a394babe538b9891
AndreaTicona2024 <aguilar.andrea.8026@gmail.com>
2024-09-22 23:39:37 -0400

    modulo ventas models control-view

--- app/Controllers/venta.php
+++ app/Controllers/venta.php
@@ -122,3 +122,25 @@ switch ($_GET["op"]) {
 				$rspta=$articulo->listarActivosVenta();
 		$data=Array();
 
+		while ($reg=$rspta->fetch_object()) {
+			$data[]=array(
+            "0"=>'<button class="btn btn-warning" onclick="agregarDetalle('.$reg->idarticulo.',\''.$reg->nombre.'\','.$reg->precio_venta.')"><span class="fa fa-plus"></span></button>',
+            "1"=>$reg->nombre,
+            "2"=>$reg->categoria,
+            "3"=>$reg->codigo,
+            "4"=>$reg->stock,
+            "5"=>$reg->precio_venta,
+            "6"=>"<img src='../files/articulos/".$reg->imagen."' height='50px' width='50px'>"
+          
+              );
+		}
+		$results=array(
+             "sEcho"=>1,//info para datatables
+             "iTotalRecords"=>count($data),//enviamos el total de registros al datatable
+             "iTotalDisplayRecords"=>count($data),//enviamos el total de registros a visualizar
+             "aaData"=>$data); 
+		echo json_encode($results);
+
+				break;
+}
+ ?>
--- app/Models/Venta.php
+++ app/Models/Venta.php
@@ -6,6 +6,24 @@ class Articulo{
 public function __construct(){
 
 }
+//listar registros 
+public function listar(){
+	$sql="SELECT a.idarticulo,a.idcategoria,c.nombre as categoria,a.codigo, a.nombre,a.stock,a.descripcion,a.imagen,a.condicion FROM articulo a INNER JOIN Categoria c ON a.idcategoria=c.idcategoria";
+	return ejecutarConsulta($sql);
+}
+
+//listar registros activos
+public function listarActivos(){
+	$sql="SELECT a.idarticulo,a.idcategoria,c.nombre as categoria,a.codigo, a.nombre,a.stock,a.descripcion,a.imagen,a.condicion FROM articulo a INNER JOIN Categoria c ON a.idcategoria=c.idcategoria WHERE a.condicion='1'";
+	return ejecutarConsulta($sql);
+}
+
+//implementar un metodo para listar los activos, su ultimo precio y el stock(vamos a unir con el ultimo registro de la tabla detalle_ingreso)
+public function listarActivosVenta(){
+	$sql="SELECT a.idarticulo,a.idcategoria,c.nombre as categoria,a.codigo, a.nombre,a.stock,(SELECT precio_venta FROM detalle_ingreso WHERE idarticulo=a.idarticulo ORDER BY iddetalle_ingreso DESC LIMIT 0,1) AS precio_venta,a.descripcion,a.imagen,a.condicion FROM articulo a INNER JOIN Categoria c ON a.idcategoria=c.idcategoria WHERE a.condicion='1'";
+	return ejecutarConsulta($sql);
+}
+}
 
 public function insertar($idcategoria,$codigo,$nombre,$stock,$descripcion,$imagen){
 	$sql="INSERT INTO articulo (idcategoria,codigo,nombre,stock,descripcion,imagen,condicion)
@@ -33,22 +51,5 @@ public function mostrar($idarticulo){
 	return ejecutarConsultaSimpleFila($sql);
 }
 
-//listar registros 
-public function listar(){
-	$sql="SELECT a.idarticulo,a.idcategoria,c.nombre as categoria,a.codigo, a.nombre,a.stock,a.descripcion,a.imagen,a.condicion FROM articulo a INNER JOIN Categoria c ON a.idcategoria=c.idcategoria";
-	return ejecutarConsulta($sql);
-}
 
-//listar registros activos
-public function listarActivos(){
-	$sql="SELECT a.idarticulo,a.idcategoria,c.nombre as categoria,a.codigo, a.nombre,a.stock,a.descripcion,a.imagen,a.condicion FROM articulo a INNER JOIN Categoria c ON a.idcategoria=c.idcategoria WHERE a.condicion='1'";
-	return ejecutarConsulta($sql);
-}
-
-//implementar un metodo para listar los activos, su ultimo precio y el stock(vamos a unir con el ultimo registro de la tabla detalle_ingreso)
-public function listarActivosVenta(){
-	$sql="SELECT a.idarticulo,a.idcategoria,c.nombre as categoria,a.codigo, a.nombre,a.stock,(SELECT precio_venta FROM detalle_ingreso WHERE idarticulo=a.idarticulo ORDER BY iddetalle_ingreso DESC LIMIT 0,1) AS precio_venta,a.descripcion,a.imagen,a.condicion FROM articulo a INNER JOIN Categoria c ON a.idcategoria=c.idcategoria WHERE a.condicion='1'";
-	return ejecutarConsulta($sql);
-}
-}
- ?>
+?>
