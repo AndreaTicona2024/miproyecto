109821915438326adcae28d09e9a95841ad160ee
AndreaTicona2024 <aguilar.andrea.8026@gmail.com>
2024-10-11 22:53:53 -0400

    modulo reportes p.v.

---
+++ app/Models/reportes/factura.php
@@ -0,0 +1,138 @@
+<?php
+require('../fpdf181/fpdf.php');
+define('EURO', chr(128) );
+define('EURO_VAL', 6.55957 );
+
+// Xavier Nicolay 2004
+// Version 1.02
+
+//////////////////////////////////////
+// Public functions                 //
+//////////////////////////////////////
+//  function sizeOfText( $texte, $larg )
+//  function addSociete( $nom, $adresse )
+//  function fact_dev( $libelle, $num )
+//  function addDevis( $numdev )
+//  function addFacture( $numfact )
+//  function addDate( $date )
+//  function addClient( $ref )
+//  function addPageNumber( $page )
+//  function addClientAdresse( $adresse )
+//  function addReglement( $mode )
+//  function addEcheance( $date )
+//  function addNumTVA($tva)
+//  function addReference($ref)
+//  function addCols( $tab )
+//  function addLineFormat( $tab )
+//  function lineVert( $tab )
+//  function addLine( $ligne, $tab )
+//  function addRemarque($remarque)
+//  function addCadreTVAs()
+//  function addCadreEurosFrancs()
+//  function addTVAs( $params, $tab_tva, $invoice )
+//  function temporaire( $texte )
+
+class PDF_Invoice extends FPDF
+{
+// private variables
+var $colonnes;
+var $format;
+var $angle=0;
+
+// private functions
+function RoundedRect($x, $y, $w, $h, $r, $style = '')
+{
+    $k = $this->k;
+    $hp = $this->h;
+    if($style=='F')
+        $op='f';
+    elseif($style=='FD' || $style=='DF')
+        $op='B';
+    else
+        $op='S';
+    $MyArc = 4/3 * (sqrt(2) - 1);
+    $this->_out(sprintf('%.2F %.2F m',($x+$r)*$k,($hp-$y)*$k ));
+    $xc = $x+$w-$r ;
+    $yc = $y+$r;
+    $this->_out(sprintf('%.2F %.2F l', $xc*$k,($hp-$y)*$k ));
+
+    $this->_Arc($xc + $r*$MyArc, $yc - $r, $xc + $r, $yc - $r*$MyArc, $xc + $r, $yc);
+    $xc = $x+$w-$r ;
+    $yc = $y+$h-$r;
+    $this->_out(sprintf('%.2F %.2F l',($x+$w)*$k,($hp-$yc)*$k));
+    $this->_Arc($xc + $r, $yc + $r*$MyArc, $xc + $r*$MyArc, $yc + $r, $xc, $yc + $r);
+    $xc = $x+$r ;
+    $yc = $y+$h-$r;
+    $this->_out(sprintf('%.2F %.2F l',$xc*$k,($hp-($y+$h))*$k));
+    $this->_Arc($xc - $r*$MyArc, $yc + $r, $xc - $r, $yc + $r*$MyArc, $xc - $r, $yc);
+    $xc = $x+$r ;
+    $yc = $y+$r;
+    $this->_out(sprintf('%.2F %.2F l',($x)*$k,($hp-$yc)*$k ));
+    $this->_Arc($xc - $r, $yc - $r*$MyArc, $xc - $r*$MyArc, $yc - $r, $xc, $yc - $r);
+    $this->_out($op);
+}
+
+function _Arc($x1, $y1, $x2, $y2, $x3, $y3)
+{
+    $h = $this->h;
+    $this->_out(sprintf('%.2F %.2F %.2F %.2F %.2F %.2F c ', $x1*$this->k, ($h-$y1)*$this->k,
+                        $x2*$this->k, ($h-$y2)*$this->k, $x3*$this->k, ($h-$y3)*$this->k));
+}
+
+function Rotate($angle, $x=-1, $y=-1)
+{
+    if($x==-1)
+        $x=$this->x;
+    if($y==-1)
+        $y=$this->y;
+    if($this->angle!=0)
+        $this->_out('Q');
+    $this->angle=$angle;
+    if($angle!=0)
+    {
+        $angle*=M_PI/180;
+        $c=cos($angle);
+        $s=sin($angle);
+        $cx=$x*$this->k;
+        $cy=($this->h-$y)*$this->k;
+        $this->_out(sprintf('q %.5F %.5F %.5F %.5F %.2F %.2F cm 1 0 0 1 %.2F %.2F cm',$c,$s,-$s,$c,$cx,$cy,-$cx,-$cy));
+    }
+}
+
+function _endpage()
+{
+    if($this->angle!=0)
+    {
+        $this->angle=0;
+        $this->_out('Q');
+    }
+    parent::_endpage();
+}
+
+// public functions
+function sizeOfText( $texte, $largeur )
+{
+    $index    = 0;
+    $nb_lines = 0;
+    $loop     = TRUE;
+    while ( $loop )
+    {
+        $pos = strpos($texte, "\n");
+        if (!$pos)
+        {
+            $loop  = FALSE;
+            $ligne = $texte;
+        }
+        else
+        {
+            $ligne  = substr( $texte, $index, $pos);
+            $texte = substr( $texte, $pos+1 );
+        }
+        $length = floor( $this->GetStringWidth( $ligne ) );
+        $res = 1 + floor( $length / $largeur) ;
+        $nb_lines += $res;
+    }
+    return $nb_lines;
+}
+
+// Company
